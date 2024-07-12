d2d522c89622571d3cd90a295836a8b18253689f
Salanoid <salanoid@gmail.com>
2024-07-13 02:36:49 +0300

    add an alias for vim in .zshrc so you can open neovim with vim command in terminal

--- installers/install-zsh.sh
+++ installers/install-zsh.sh
@@ -1,14 +1,18 @@
+ZSH_FILE=~/.zshrc
+ACTIVATE_LINE_STRING='eval "$(/usr/bin/mise activate zsh)"'
+ALIAS_FOR_VIM='alias vim=nvim'
 sudo apt install build-essential curl file git
 sudo apt install zsh
 chsh -s $(which zsh)
 sudo apt install git-core curl fonts-powerline
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-ACTIVATE_LINE_STRING='eval "$(/usr/bin/mise activate zsh)"'
-ZSH_FILE=~/.zshrc
-
 if ! grep -qF "$ACTIVATE_LINE_STRING" "$ZSH_FILE"; then
   echo "$ACTIVATE_LINE_STRING" >>"$ZSH_FILE"
+fi
+
+if ! grep -qF "$ALIAS_FOR_VIM" "$ZSH_FILE"; then
+  echo "$ALIAS_FOR_VIM" >>"$ZSH_FILE"
 fi
 
 exec /bin/zsh
